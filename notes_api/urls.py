8b597235cb690cb89e915fd7f91e7d0f6d3c5544
Reyan Khan <reyankhan735@gmail.com>
2026-03-13 16:14:52 +0530

    added JWT

--- notes_api/urls.py
+++ notes_api/urls.py
@@ -20,12 +20,19 @@ from rest_framework.routers import DefaultRouter
 from notes.views import NotesViewSet
 from accounts.views import UserViewSet
 
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 router = DefaultRouter()
 router.register(r'notes',NotesViewSet)
 router.register(r'users',UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 urlpatterns += router.urls
