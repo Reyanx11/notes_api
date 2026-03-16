b2a780c3f166fa4faed11681645c08a7135c3f4f
Reyan Khan <reyankhan735@gmail.com>
2026-03-16 10:27:47 +0530

    added a basic custom middleware

---
+++ notes_api/middleware.py
@@ -0,0 +1,12 @@
+class MyMiddleWare:
+    def __init__(self,get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        print("before view")
+
+        response = self.get_response(request)
+        print("after view")
+        
+        return response
--- notes_api/settings.py
+++ notes_api/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'notes_api.middleware.MyMiddleWare',
 ]
 
 ROOT_URLCONF = 'notes_api.urls'
